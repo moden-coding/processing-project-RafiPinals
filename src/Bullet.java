a8da22389dda43da2eacd52601163a8da4e839dd
RafiPinals <rafipinals6@gmail.com>
2024-03-08 13:34:26 -0500

    Submission before Classes

---
+++ src/Bullet.java
@@ -0,0 +1,16 @@
+import processing.core.*;
+
+public class Bullet {
+    float x;
+    float y;
+
+    public Bullet(PApplet p, float x, float y){
+        this.x=x;
+        this.y=y;
+        canvase=p;
+
+    }
+
+
+
+}
